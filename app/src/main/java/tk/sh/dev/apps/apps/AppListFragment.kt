ab20b826529cd29df8643d19649a2742d2b64a36
shinya-takano <dev.shinya.takano@gmail.com>
2020-10-08 00:12:25 +0900

    create recyclerview skeleton - created package manager by repository pattern

--- app/src/main/java/tk/sh/dev/apps/apps/AppListFragment.kt
+++ app/src/main/java/tk/sh/dev/apps/apps/AppListFragment.kt
@@ -1,16 +1,25 @@
 package tk.sh.dev.apps.apps
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import tk.sh.dev.apps.databinding.FragmentAppListBinding
+import tk.sh.dev.apps.repository.PackageManagerRepository
+
 
 class AppListFragment : Fragment() {
 
     private var _binding: FragmentAppListBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: AppListViewModel
+
+    private lateinit var adapter: AppListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +28,27 @@ class AppListFragment : Fragment() {
     ): View? {
         _binding = FragmentAppListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(requireActivity())
+            adapter = AppListAdapter()
+        }
+        adapter = binding.recyclerView.adapter as AppListAdapter
+
+        viewModel = AppListViewModel(PackageManagerRepository(requireContext().applicationContext))
+        val packages = viewModel.getInstalledPackages()
+
+        val list = arrayListOf<App>()
+        for (onePackage in packages) {
+            Log.d("tag", "$onePackage")
+            val app = App(null, onePackage.applicationInfo.packageName, onePackage.versionName)
+            list.add(app)
+        }
+        adapter.updateList(list)
     }
 
     override fun onDestroyView() {
